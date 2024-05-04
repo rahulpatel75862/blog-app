d44252ea4eb610f9b444bb74f0eeb1750338ac38
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-04 15:24:19 +0530

    header section created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import About from './pages/About'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element= {<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
       </Routes>
     </BrowserRouter>
   )
