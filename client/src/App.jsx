9d3a3b2846b09a9c0787035d1ef30536078217b8
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-05 20:57:41 +0530

    making the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Header from './components/Header';
-import Projects from './pages/Projects';
-import FooterCom from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
+import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -16,16 +17,18 @@ const App = () => {
       <Header />
       <ToastContainer />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/projects' element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
