cc5c80fe0d04200b3e387738cc7b108978c3bda8
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-09 19:51:55 +0530

    update user changes done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,46 +1,57 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 export const test = (req, res) => {
-    res.json({ message: 'API is working'});
-}
+  res.json({ message: "API is working" });
+};
 
 export const updateUser = async (req, res, next) => {
-    if(req.user.id !== req.params.userId){
-        return next(errorHandler(403, 'You are not allowed to update that user'));
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update that user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must be atleast 6 characters"));
     }
-    if(req.body.password){
-        if(req.body.password.length<6){
-            return next(errorHandler(400, 'Password must be atleast 6 characters'));
-        }
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.userName) {
+    if (req.body.userName.length < 7 || req.body.userName.length > 20) {
+      return next(
+        errorHandler(
+          400,
+          "userName must be between 7 and 20 characters of length"
+        )
+      );
     }
-    if(req.body.userName){
-        if(req.body.userName.length<7 || req.body.userName.length>20){
-            return next(errorHandler(400, 'userName must be between 7 and 20 characters of length'));
-        }
+    if (req.body.userName.includes(" ")) {
+      return next(errorHandler(400, "UserName cannot contain spaces"));
     }
-    if(req.body.userName.includes(' ')){
-        return next(errorHandler(400, 'UserName cannot contain spaces'));
+    if (req.body.userName !== req.body.userName.toLowerCase()) {
+      return next(errorHandler(400, "userName Must be lowerCase"));
     }
-    if(req.body.userName !== req.body.userName.toLowerCase()){
-        return next(errorHandler(400, 'userName Must be lowerCase'));
+    if (!req.body.userName.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "userName can only contain letters and numbers")
+      );
     }
-    if(!req.body.userName.match(/^[a-zA-Z0-9]+$/)){
-        return next(errorHandler(400, 'userName can only contain letters and numbers'));
-    }
-    try{
-        const updateUser = await User.findByIdAndUpdate(req.params.userId, {
-            $set: {
-                userName: req.body.userName,
-                email:req.body.email,
-                profilePicture:req.body.profilePicture,
-                password:req.body.password,
-            }
-        }, { new: true});
-        const { password, ...rest } = updateUser._doc;
-        res.status(200).json(rest);
-    } catch(error){
-        next(error)
-    }
-}
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          userName: req.body.userName,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, TextInput } from "flowbite-react";
 import { useState, useRef, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import {
   getDownloadURL,
   getStorage,
@@ -8,16 +9,20 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../Fierbase";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { updateFailure, updateStart, updateSuccess } from "../redux/user/userSlice";
 
 const DashProfile = () => {
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadingProgress, setImageFileUploadingProgress] =
     useState(null);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploadSucces, setImageFileUploadSuccess] = useState(false)
+  const [formData, setFormData] = useState({});
   const profilePicRef = useRef();
 
   const handleImageChange = (e) => {
@@ -35,6 +40,7 @@ const DashProfile = () => {
   }, [imageFile]);
 
   const uploadImage = async () => {
+    setImageFileUploadSuccess(true)
     setImageFileUploadError(null);
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
@@ -54,19 +60,59 @@ const DashProfile = () => {
         setImageFileUploadingProgress(null);
         setImageFile(null);
         setImageFileUrl(null);
+        setImageFileUploadSuccess(false);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
           setImageFileUrl(downloadUrl);
+          setFormData({ ...formData, profilePicture: downloadUrl });
+          setImageFileUploadSuccess(false);
         });
       }
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(Object.keys(formData).length === 0){
+      return;
+    }
+    if(imageFileUploadSucces){
+      return;
+    }
+    try{
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(!res.ok){
+        toast.error(`${data.message}`)
+        dispatch(updateFailure(data.message));
+      }
+      else{
+        toast.success('User Information updated Successfully!!')
+        dispatch(updateSuccess(data));
+      }
+    } catch(error){
+      toast.error(`Internal Server Error!!`)
+      dispatch(updateFailure(error.message));
+    }
+  }
+
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           accept="image/*"
@@ -78,28 +124,35 @@ const DashProfile = () => {
           className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
           onClick={() => profilePicRef.current.click()}
         >
-        {imageFileUploadingProgress && (
-          <CircularProgressbar value={imageFileUploadingProgress || 0} 
-          text= {`${imageFileUploadingProgress}%`}
-            strokeWidth={5}
-            styles={{
-              root:{
-                width: '100%',
-                height: '100%',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-              },
-              path: {
-                stroke: `rgba(62,152,199, ${imageFileUploadingProgress /100})`,
-              },
-            }}
-          />
-        )}
+          {imageFileUploadingProgress && (
+            <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62,152,199, ${
+                    imageFileUploadingProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className={`rounded-full w-full h-full border-8 border-[lightgray] object-cover ${imageFileUploadingProgress && imageFileUploadingProgress <100 && 'opacity-60'}`}
+            className={`rounded-full w-full h-full border-8 border-[lightgray] object-cover ${
+              imageFileUploadingProgress &&
+              imageFileUploadingProgress < 100 &&
+              "opacity-60"
+            }`}
           />
         </div>
         {imageFileUploadError && (
@@ -110,14 +163,21 @@ const DashProfile = () => {
           id="userName"
           placeholder="userName"
           defaultValue={currentUser.userName}
+          onChange={handleChange}
         />
         <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type="password" id="password" placeholder="password" />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
