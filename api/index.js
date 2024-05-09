4415e08edcce2cd6c9e889f8a64899d1cffe1f14
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-09 17:18:13 +0530

    updating user info backend completed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
