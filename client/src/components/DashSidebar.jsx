184d73edad12ac9f7f7438cffc15e05f8bdb3a5f
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-10 13:20:26 +0530

    add posts section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,12 +1,13 @@
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {  signOutSucces } from "../redux/user/userSlice";
+import { signOutSucces } from "../redux/user/userSlice";
 import { toast } from "react-toastify";
-import {  useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const DashSidebar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -19,41 +20,55 @@ const DashSidebar = () => {
     }
   }, [location.search]);
 
-  const  handleSignOut = async() => {
-    try{
-      const res = await fetch('/api/user/signout', {
-        method: 'POST'
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
       });
       const data = await res.json();
-      if(!res.ok){
-        toast.error(`${data.message}`)
+      if (!res.ok) {
+        toast.error(`${data.message}`);
         return;
-      }
-      else{
-        toast.success('Signed Out Successfully!!')
+      } else {
+        toast.success("Signed Out Successfully!!");
         dispatch(signOutSucces());
       }
-    } catch(error){
+    } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"user"}
+              label={currentUser ? "Admin" : "user"}
               labelColor="dark"
-              as = 'div'
+              as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" onClick = {handleSignOut}>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignOut}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
