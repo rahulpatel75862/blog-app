652cbc36b689c0c59f5b6af0ed4f67182dc6a140
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-12 14:36:48 +0530

    added recent article section

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [post, setPost] = useState(null);
+  const [recentPost, setRecentPost] = useState(null);
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -27,6 +29,22 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchPost = async () => {
+        const res = await fetch(`/api/post/get/posts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPost(data.posts);
+        }
+      };
+      fetchPost();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -52,15 +70,25 @@ const PostPage = () => {
         className="mt-10 p-3 max-h-[600px] w-full object-cover"
       />
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
-        <span>{post&&new Date(post.createdAt).toLocaleDateString()}</span>
-        <span>{post&&(post.content.length/1000).toFixed(0)} mins read</span>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
       </div>
-      <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}>
-      </div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
       <div className="max-w-4xl mx-auto w-full">
-        <CallToAction/>
+        <CallToAction />
       </div>
-      <CommentSection postId = {post._id}/>
+      <CommentSection postId={post._id} />
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 justify-center">
+          { recentPost && recentPost.map((post) => {
+            return <PostCard key={post._id} post = {post}/>
+          })}
+        </div>
+      </div>
     </main>
   );
 };
