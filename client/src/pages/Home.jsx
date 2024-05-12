f01fa28fe63543813eca1dd4db91145c74ad8937
rahulpatel75862 <patelrahul9533@gmail.com>
2024-05-12 21:41:29 +0530

    home page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,61 @@
-
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import PostCard from "../components/PostCard";
+import { useEffect, useState } from "react";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try{
+        const res = await fetch('/api/post/get/posts')
+        const data = await res.json();
+        if(res.json){
+          setPosts(data.posts);
+        }
+      } catch(error){
+        console.log(error.message)
+      }
+    }
+    fetchPosts()
+  }, [])
 
-export default Home
+  return (
+    <div className="">
+      <div className="flex flex-col gap-6 lg:p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Dive into a world of thought-provoking content spanning technology,
+          culture, and more. Discover articles that inspire curiosity and foster
+          learning, enriching your journey through insightful exploration and
+          diverse perspectives.
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length>0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              { posts.map((post) => (
+                <PostCard key={post._id} post={post}/>
+              ))}
+            </div>
+            <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
